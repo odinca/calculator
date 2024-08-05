b40a335db238b86b2190d0a4e1d9073b366222f9
Chris <173185832+odinca@users.noreply.github.com>
2024-08-05 17:57:33 +0100

    Create function to call operator functions

--- script.js
+++ script.js
@@ -12,4 +12,19 @@ function divide(num1, num2) {
     return num1 / num2
 }
 
-//
+// Operation variables
+let firstNum
+let secondNum
+let operator
+
+function operate(first, second, oper) {
+    const operators = [
+        { type: "+", func: add,},
+        { type: "-", func: subtract, },
+        { type: "*", func: multiply, },
+        { type: "/", func: divide, }
+    ]
+    for (let op of operators) {
+        if (op.type == oper) {return op.func(first, second)}
+    };
+}
