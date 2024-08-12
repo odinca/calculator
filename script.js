c33d99c3babe134cb1c46ccd92dd2cf1b30033d3
Chris <173185832+odinca@users.noreply.github.com>
2024-08-12 21:05:55 +0100

    Clean up existing code. Add control for stringing together multiple operators

--- script.js
+++ script.js
@@ -26,9 +26,10 @@ let displayData = "";
 let selectedOperator = "";
 
 function provideSum() {
-    secondNum = Number(displayData.split(selectedOperator)[1]);
+    secondNum = Number(displayData);
     displayData = "";
     onDisplay(operate(firstNum, secondNum, selectedOperator));
+    selectedOperator = "";
 }
 
 function onDisplay(selectedBtn) {
@@ -50,16 +51,21 @@ function operate(first, second, oper) {
 }
 
 function onOpClick(opToAdd) {
+    if (selectedOperator.length > 0) {
+        provideSum();
+    }
     firstNum = Number(displayData);
-    onDisplay(opToAdd);
+    displayData = "";
     selectedOperator = opToAdd;
 }
 
+// Function to control what happens when a number is pressed
 function getNumButton() {
     let numButtons = document.querySelectorAll("button.number");
     numButtons.forEach(numButton => numButton.addEventListener("click", () => onDisplay(numButton.textContent)));
 }
 
+// Function to control what happens when an operator is pressed
 function getOperator() {
     let opButtons = document.querySelectorAll(".operator");
     opButtons.forEach(opButton => opButton.addEventListener("click", () => onOpClick(opButton.textContent)));
@@ -71,5 +77,6 @@ getOperator();
 const equals = document.querySelector("#equals");
 const clear = document.querySelector("#clear");
 
+// Control what happens when equals or clear buttons are pressed
 equals.addEventListener("click", () => provideSum());
 clear.addEventListener("click", () => onDisplay("clear"));
